54f1fdcf8d3486f73751a847f9b31217cdb2d4b2
Чингис Чингис <gikipiki19@gmail.com>
2021-11-24 17:45:32 +0700

    Добавьте файлы проекта.

---
+++ ApplicationForVacancy/Context/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using ApplicationForVacancy.Models;
+
+namespace ApplicationForVacancy.Context
+{
+    public class ApplicationContext: DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+
+    }
+    
+}
---
+++ ApplicationForVacancy/Startup.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApplicationForVacancy.Service;
+
+namespace ApplicationForVacancy
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //Регистрация сервиса
+            services.AddTransient<IAbstract, Entity>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello World!");
+            //    });
+            //});
+        }
+    }
+}
